69bdd4b623d2fc8e42d6d41af33c7b06eedf4714
godcreator333 <haripreetham.jntuh@gmail.com>
2024-02-03 13:42:25 +0530

    working

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import "./navbarstyles.css"
-import logo_img from './assets/images/logo_white.png'
+import logo_img from '../assets/images/logo_white.png'
 
 function Navbar() {
     return (
         <div className='all-nav-bar'>
 
-            <span className='navbar_logo'>
-                <img src={logo_img} alt='logo_img' />
+            <span>
+                <img className='navbar_logo' src={logo_img} alt='logo_img'/>
             </span>
 
             <div className='navbar_content'>
                 <a href='#/'>Home</a>
                 <a href='#allblogs'>All Blogs</a>
                 <a href='#contact'>Contact</a>
-
             </div>
         </div>
     )
