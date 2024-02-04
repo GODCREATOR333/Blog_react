1e20671ff3fde2b137cb7ab9eb937a36fcdededa
godcreator333 <haripreetham.jntuh@gmail.com>
2024-02-05 00:35:22 +0530

    added nav bar and footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <ul className='footer__categories'>
+        <li><Link to='posts/categories/coding'>Coding</Link></li>
+        <li><Link to='posts/categories/maths'>Maths</Link></li>
+        <li><Link to='posts/categories/physics'>Physics</Link></li>
+        <li><Link to='posts/categories/literature'>Literature</Link></li>
+        <li><Link to='posts/categories/investment'>Investment</Link></li>
+        <li><Link to='posts/categories/astronomy'>Astronomy</Link></li>
+        <li><Link to='posts/categories/agriculture'>Agriculture</Link></li>
+      </ul>
+
+      <div className='footer_copyright'>
+        <small>All Rights Reserved  &copy; Copyright , GodCreator333 </small>
+
+      </div>
+    </footer>
   )
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png'
-import { FaBars } from 'react-icons/fa'
+// import { FaBars } from 'react-icons/fa'
 import { AiOutlineClose } from 'react-icons/ai'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin,faGithub} from '@fortawesome/free-brands-svg-icons'
+
+
 function Navbar() {
     return (
         <nav>
-            <div classname="container nav__container" >
+            <div className="container nav__container" >
 
-                <Link to="/" classname='nav_logo'>
-                    <img src={logo} alt='logo' />
+                <Link to="/" className='nav_logo'>
+                    <img  src={logo} alt='logo' />
                 </Link>
 
-                <ul>
+                <ul className='nav_menu'>
                     <li><Link to='/allposts'>All Posts</Link></li>
                     <li><Link to='/about'>About</Link></li>
                     <li><Link to='/contact'>Contact</Link></li>
+                    <li>
+                        <a href='https://www.linkedin.com/in/hari-preetham-934789201/' target="_blank" rel="noopener noreferrer">
+                             <FontAwesomeIcon icon={faLinkedin} size='2x'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://github.com/GODCREATOR333' target="_blank" rel="noopener noreferrer">
+                             <FontAwesomeIcon icon={faGithub} size='2x'/>
+                        </a>
+                    </li>
                 </ul>
 
-                <button className='nav__toggle-btn'>
+                
+                <button className='nav_toggle-btn'>
                     <AiOutlineClose />
                 </button>
 
