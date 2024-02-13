7292f1612f7204ed53530cefe1dd4d64c533fe66
GODCREATOR333 <preetham.chitti.9@gmail.com>
2024-02-13 19:59:21 +0530

    added hero component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,15 @@ import Posts from '../Components/Posts'
 function Home() {
     return (
         <div>
+            <section className='hero'>
+                <div className=' container hero_content'>
+
+                </div>
+            </section>
+
+
+
+
             <Posts />
         </div>
     )
