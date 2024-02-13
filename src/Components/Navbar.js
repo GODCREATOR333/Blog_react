37b701c007c7461621464b2c91dc9a770cfbd104
GODCREATOR333 <preetham.chitti.9@gmail.com>
2024-02-13 18:36:14 +0530

    added media queires and all other styling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,40 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png'
-// import { FaBars } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import { AiOutlineClose } from 'react-icons/ai'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin,faGithub} from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
 function Navbar() {
+
+    const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false);
+
+    const closeNavHandler = () => {
+        if (window.innerWidth < 800) {
+            setIsNavShowing(false);
+        } else {
+            setIsNavShowing(true)
+        }
+    }
+
     return (
         <nav>
             <div className="container nav__container" >
 
                 <Link to="/" className='nav_logo'>
-                    <img  src={logo} alt='logo' />
+                    <img src={logo} alt='logo' onClick={closeNavHandler} />
                 </Link>
 
-                <ul className='nav_menu'>
-                    <li><Link to='/allposts'>All Posts</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
+                {isNavShowing && <ul className='nav_menu'>
+                    <li><Link to='/allposts' onClick={closeNavHandler}>All Posts</Link></li>
+                    <li><Link to='/about' onClick={closeNavHandler}>About</Link></li>
+                    <li><Link to='/contact' onClick={closeNavHandler}>Contact</Link></li>
                     <li>
                         <a href='https://www.linkedin.com/in/hari-preetham-934789201/' target="_blank" rel="noopener noreferrer">
-                             <FontAwesomeIcon icon={faLinkedin} size='2x'/>
+                            <FontAwesomeIcon icon={faLinkedin} size='2x' />
                         </a>
                     </li>
                     <li>
                         <a href='https://github.com/GODCREATOR333' target="_blank" rel="noopener noreferrer">
-                             <FontAwesomeIcon icon={faGithub} size='2x'/>
+                            <FontAwesomeIcon icon={faGithub} size='2x' />
                         </a>
                     </li>
-                </ul>
+                </ul>}
 
-                
-                <button className='nav_toggle-btn'>
-                    <AiOutlineClose />
+
+                <button className='nav_toggle-btn' onClick={() => setIsNavShowing(!isNavShowing)}>
+                    {isNavShowing ? < AiOutlineClose /> : <FaBars />}
                 </button>
 
             </div>
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 function CreatePost() {
+
+    const [title, setTitle] = useState('')
+    const [category, setCategory] = useState('uncategorized')
+    const [description, setDescription] = useState('')
+    const [thumbnail, setThumbnail] = useState('')
+
+
+
+    const POST_CATEGORIES = ['Economics', 'Maths', 'Physics', "Coding", 'Engineering', 'Literature']
+
+    const modules = {
+        toolbar: [
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+            ['link', 'image'],
+            ['code-block', { 'script': 'sub' }], // Add the code block feature to the toolbar
+            [{ 'copy-code': 'Copy Code' }], // Custom button for copying code
+            ['clean']
+        ],
+        clipboard: {
+            matchVisual: false,
+        }
+    }
+
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image',
+        'code-block' // Include the code block format
+    ]
+
     return (
-        <div>CreatePost</div>
+        <section className='create-post'>
+            <div className='container'>
+                <h2>Create a Post</h2>
+                <p className='form_error_message'> This is a error message </p>
+                <form className='form create-post-form'>
+
+                    <input type='text ' placeholder='Title' value={title} onChange={e => setTitle(e.target.value)} autoFocus />
+                    <select name='Category' value={category} onChange={e => setCategory(e.target.value)}>
+                        {
+                            POST_CATEGORIES.map(cat => <option key={cat}> {cat} </option>)
+                        }
+                    </select>
+
+                    <ReactQuill modules={modules} formats={formats} value={description} onChange={setDescription} />
+                    <input type='file' onChange={e => setThumbnail(e.target.files[0])} accept='png,jpg,jpeg' />
+                    <button type='submit' className='btn primary'> Create </button>
+
+                </form>
+
+            </div>
+
+        </section>
     )
 }
 
