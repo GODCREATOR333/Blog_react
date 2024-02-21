3156209b7596309ffe87ef57bda1c4b8a1940565
godcreator333 <haripreetham.jntuh@gmail.com>
2024-02-21 22:02:56 +0530

    added contact

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,66 @@
 import React from 'react'
+import html_logo from '../assets/logos/html.svg'
+import css_logo from '../assets/logos/css.svg';
+import javascript_logo from '../assets/logos/javascript.svg';
+import react_logo from '../assets/logos/react.svg';
+import nodejs_logo from '../assets/logos/nodejs.svg';
+import figma_logo from '../assets/logos/figma.svg';
+import mongo_logo from '../assets/logos/mongo.svg';
+import firebase_logo from '../assets/logos/firebase.svg';
+import git_logo from '../assets/logos/git.svg';
+import python_logo from '../assets/logos/python.svg';
+import tensorflow_logo from '../assets/logos/tensorflow.svg';
+import opencv_logo from '../assets/logos/opencv.svg';
+import linux_logo from '../assets/logos/linux.svg';
+import autodesk_logo from '../assets/logos/autodesk.svg';
+import ansys_logo from '../assets/logos/ansys.svg';
+import adobe_logo from '../assets/logos/adobe.svg';
+import solidworks_logo from '../assets/logos/solidworks.svg';
+import unreal_logo from '../assets/logos/unreal.svg'
+import nvidia_logo from '../assets/logos/nvidia.svg'
+import arduino_logo from '../assets/logos/arduino.svg'
+
 
 function About() {
     return (
-        <div>About</div>
+        <section>
+
+
+            <div id='about' className=' container about'>
+
+                <h1>About Me</h1>
+    
+                <p>Hey there 👋! I'm Hari Preetham, a BTech student at Jawaharlal Nehru Technological University, Hyderabad.
+                I'm an enthusiastic engineering student passionate about creating innovative solutions. With a love for mathematics, physics, coding, and design, I aim to craft remarkable technologies.
+                Beyond academics, you'll find me indulging in bike rides, football matches, and gym sessions. I possess a proficiency in skills essential to both the scientific community and the tech industry.
+                My ultimate drive is my profound passion for engineering and the boundless possibilities it offers. Let's build a fantastic future together! </p>
+            
+                <div className='logos'>
+                    <img src={html_logo} alt='html_img' />
+                    <img src={css_logo} alt='css_img' />
+                    <img src={javascript_logo} alt='js_img' />
+                    <img src={react_logo} alt='react_img' />
+                    <img src={nodejs_logo} alt='nodejs_img' />
+                    <img src={figma_logo} alt='figma_img' />
+                    <img src={mongo_logo} alt='mongo_img' />
+                    <img src={firebase_logo} alt='firebase_img' />
+                    <img src={git_logo} alt='git_img' />
+                    <img src={python_logo} alt='python_img' />
+                    <img src={tensorflow_logo} alt='tensorflow_img' />
+                    <img src={opencv_logo} alt='opencv_img' />
+                    <img src={linux_logo} alt='linux_img' />
+                    <img src={autodesk_logo} alt='autodesk_img' />
+                    <img src={adobe_logo} alt='adobe_img' />
+                    <img src={solidworks_logo} alt='solidworks_img' />
+                    <img src={unreal_logo} alt='unreal_img' />
+                    <img src={nvidia_logo} alt='nvidia_img' />
+                    <img src={arduino_logo} alt='arduino_img' />
+                    <img src={ansys_logo} alt='ansys_img' />
+                </div>
+
+
+        </div>
+        </section>
     )
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,152 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import emailjs from 'emailjs-com'
+import { useForm } from 'react-hook-form';
 
 function Contact() {
+
+    const [showAlert, setShowAlert] = useState(false);
+    const Alert = ({ onClose }) => (
+        <div className="alert">
+            <span className="submit-close" onClick={onClose} >&times;</span>
+            <p>Email sent successfully !! </p>
+        </div>
+    );
+
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        const { name, email, subject, message } = data;
+
+        emailjs.send('service_8fe0636', 'template_h5jdh1r', {
+            from_name: name,
+            from_email: email,
+            message: message,
+            subject: subject
+        }, '7mD_7ks4HAcpbyvmO')
+            .then((response) => {
+                console.log('Email sent!', response);
+                setIsSubmitted(true);
+                setShowAlert(true);
+            })
+            .catch((error) => {
+                console.error('There was an error sending the email:', error);
+            });
+    };
+
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    useEffect(() => {
+        if (isSubmitted) {
+            reset();
+            setIsSubmitted(false);
+        }
+    }, [isSubmitted, reset]);
+
+    
+
     return (
-        <div>Contact</div>
+        <section className='container'>
+            <div id='contact' className='contact'>
+            {showAlert && <Alert onClose={() => setShowAlert(false)} />}
+            <h1>Say Hello 👋!</h1>
+            <div className='info'>
+                <h2 className='h2_info'>Let's work <br /> together !</h2>
+                <p className='p_info'> <span className='info_head'>Email : </span> Haripreetham.jntuh@gmail.com <br />
+                    <span className='info_head'>Contact :</span> +917013589964</p>
+            </div>
+            <div className='ContactForm'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-12 text-center'>
+                            <div className='contactForm'>
+                                <form id='contact-form' onSubmit={handleSubmit(onSubmit)} noValidate>
+                                    {/* Row 1 of form */}
+                                    <div className='row formRow'>
+                                        <div className='col-6'>
+                                            <input
+                                                type='text'
+                                                name='name'
+                                                {...register('name', {
+                                                    required: { value: true, message: 'Please enter your name' },
+                                                    maxLength: {
+                                                        value: 30,
+                                                        message: 'Please use 30 characters or less'
+                                                    }
+                                                })}
+                                                className='form-control formInput'
+                                                placeholder='Name'
+                                            ></input>
+                                            {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
+                                        </div>
+                                        <div className='col-6'>
+                                            <input
+                                                type='email'
+                                                name='email'
+                                                {...register('email', {
+                                                    required: true,
+                                                    pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                                                })}
+                                                className='form-control formInput'
+                                                placeholder='Email address'
+                                            ></input>
+                                            {errors.email && (
+                                                <span className='errorMessage'>Please enter a valid email address</span>
+                                            )}
+                                        </div>
+                                    </div>
+                                    {/* Row 2 of form */}
+                                    <div className='row formRow'>
+                                        <div className='col'>
+                                            <input
+                                                type='text'
+                                                name='subject'
+                                                {...register('subject', {
+                                                    required: { value: true, message: 'Please enter a subject' },
+                                                    maxLength: {
+                                                        value: 75,
+                                                        message: 'Subject cannot exceed 75 characters'
+                                                    }
+                                                })}
+                                                className='form-control formInput'
+                                                placeholder='Subject'
+                                            ></input>
+                                            {errors.subject && (
+                                                <span className='errorMessage'>{errors.subject.message}</span>
+                                            )}
+                                        </div>
+                                    </div>
+                                    {/* Row 3 of form */}
+                                    <div className='row formRow'>
+                                        <div className='col'>
+                                            <textarea
+                                                rows={3}
+                                                name='message'
+                                                {...register('message', {
+                                                    required: true
+                                                })}
+                                                className='form-control formInput'
+                                                placeholder='Message'
+                                            ></textarea>
+                                            {errors.message && <span className='errorMessage'>Please enter a message</span>}
+                                        </div>
+                                    </div>
+                                    <button className='submit-btn' type='submit'>
+                                        Submit
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
     )
 }
 
