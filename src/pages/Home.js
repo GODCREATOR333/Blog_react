47819d67eb9f5dc2fef69d6c6f91384b1239c2b7
GodCreator333 <haripreetham.jntuh@gmail.com>
2024-02-22 06:22:10 +0530

    added all styles in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ function Home() {
 
                 <div className='left-side-info'>
                     
-                    <img className='gifs coding_gif' 
+                    {/* <img className='gifs coding_gif' 
                             src={coding_gif}
                             alt='coding_gif'/>
                 
@@ -23,24 +23,37 @@ function Home() {
 
                     <img className='gifs coding_gif' 
                             src={coding_gif}
-                            alt='coding_gif'/>
+                            alt='coding_gif'/> */}
 
-                    <div className='hero_text'>
-                        <h1>Welcome!</h1>
-                        <h2>This is my personal blog</h2>
-                        <h3>This is like a digital collection of all my inventions 
+                    <div className='container-home '>
+                        <h1  className='hero_text hero__h1' > Welcome!</h1>
+                        <h2 className='hero_text hero__h2'> This is my personal blog</h2>
+                        <h3 className='hero_text hero__h3'> This is like a digital collection of all my inventions 
                             and acts like a catalogue of my projects. 
                             <br/>
                             This is a place where i store as well as share my Ideas,
                             Creations & Thoughts 
                         </h3>
+                        <div className='link_home'>
+                        <p className='hero_text hero__p'> Check out my Portifolio website here @ </p>
+                        
+                        <div className='buttons'>
+                            <a href="" target="_blank" rel="noopener noreferrer">
+                                <button className='button_hero'>Portifolio<span style={{ fontSize: '18px' }}> ↗ </span></button>
+                            </a>
+                        </div>
+
+                        </div>
+                        
+
+
                     </div>
 
                 </div>
 
                 <div className='right-side-img'></div>
                 
-
+                
 
 
 
